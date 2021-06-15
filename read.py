9b8a46dda4f4d39c054df02922f1aa7d89289306
Eva Sung <syhsan215@outlook.com>
2021-06-15 16:47:39 +0800

    update read.py

--- read.py
+++ read.py
@@ -5,9 +5,21 @@ with open("reviews.txt","r")as f:
 	for line in f:
 		count += 1
 		data.append(line.strip())
-		if count % 10000 == 0:
-			print(len(data))
+		#if count % 10000 == 0:
+			#print(len(data))
 
 
-# print(len(data))
+
+print("讀取完了，總共有",len(data),"筆留言")
+
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+
+
+print("每筆留言平均長度為：",sum_len/len(data))
+
+
 # print(data[0])
